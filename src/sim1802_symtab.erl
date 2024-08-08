ca022e98b6277769dbaa2fcaa2f6a192f9321f09
Mikael Pettersson <mikpelinux@gmail.com>
2024-08-08 18:06:41 +0200

    sim1802_symtab: map address to symbol+offset

---
+++ src/sim1802_symtab.erl
@@ -0,0 +1,46 @@
+%%% -*- erlang-indent-level: 2 -*-
+%%%
+%%% Symbol table for CDP1802 simulator
+
+-module(sim1802_symtab).
+
+-export([ init/1
+        , resolve/2
+        ]).
+
+-export_type([ address/0
+             , name/0
+             , offset/0
+             , symtab/0
+             ]).
+
+-type address() :: non_neg_integer().
+-type name() :: string().
+-type offset() :: non_neg_integer().
+-type symtab() :: false | ets:tid().
+
+%% API =========================================================================
+
+-spec init([{name(), address()}]) -> symtab().
+init([]) -> false;
+init(Syms) ->
+  ETS = ets:new(undefined, [ordered_set]),
+  lists:foreach(
+    fun({Name, Address}) ->
+      ets:insert(ETS, {Address, Name})
+    end, Syms),
+  ETS.
+
+-spec resolve(symtab(), address()) -> false | {name(), offset()}.
+resolve(false, _Address) -> false;
+resolve(ETS, Address) ->
+  case ets:lookup(ETS, Address) of
+    [{_Address, Name}] -> {Name, 0};
+    [] ->
+      case ets:prev(ETS, Address) of
+        '$end_of_table' -> false;
+        Base ->
+          Name = ets:lookup_element(ETS, Base, 2),
+          {Name, Address - Base}
+      end
+  end.
